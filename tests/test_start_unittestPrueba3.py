a7bd30560d860e74c8cffed1fd987e274ac554f1
Fabian <buitronkevin2010@gmail.com>
2024-08-06 19:14:20 -0600

    chore: a ver jiji

--- tests/test_start_unittestPrueba3.py
+++ tests/test_start_unittestPrueba3.py
@@ -13,7 +13,7 @@ class TestStart(unittest.TestCase):
         self.driver.quit()
 
     def test_find_h1(self):
-        self.driver.get("https://leofabro.github.io/PaginaPruebas/")
+        self.driver.get("https://favofrito3.github.io/PaginaPruebas/")
         # Esperar hasta que el elemento <h1> esté presente
         element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.TAG_NAME, "h1"))
@@ -21,7 +21,7 @@ class TestStart(unittest.TestCase):
         self.assertIsNotNone(element)
 
     def test_find_paragraph(self):
-        self.driver.get("https://leofabro.github.io/PaginaPruebas/")
+        self.driver.get("https://favofrito3.github.io/PaginaPruebas/")
         # Esperar hasta que el elemento <p> esté presente
         element = WebDriverWait(self.driver, 10).until(
             EC.presence_of_element_located((By.TAG_NAME, "p"))
